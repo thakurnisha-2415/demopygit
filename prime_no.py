6a7751eb542b97b1b49966a01ca6c8cee0755e7a
NISHA THAKUR <80372711+thakurnisha-2415@users.noreply.github.com>
2025-04-05 07:57:08 -1100

    Delete prime_no.py

--- prime_no.py
+++
@@ -1,8 +0,0 @@
-n=int(input("enter the no.: "))
-for i in range(2,n):
-    if (n%i==0):
-        break
-if i+1==n:
-    print(n, " the no. is prime no.")
-else:
-    print(n, " the no. is not prime")
